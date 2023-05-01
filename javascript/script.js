71e18f41a78330c43cacb5352bcdbdefd33f10fa
RakShaDhami <116144642+RakShaDhami@users.noreply.github.com>
2023-05-01 22:20:38 +0545

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('#nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav(){
+    if(window.scrollY > nav.offsetHeight){
+        nav.classList.add('active')
+    }
+    else{
+        nav.classList.remove('active')
+   }
+}
